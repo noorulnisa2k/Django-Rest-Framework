392c0b3dfeba6f030ddc7f8fee6fb8c41c9af70a
NoorulNisa <gsnoor2k@gmail.com>
2024-01-29 07:04:49 +0500

    Initial code setup

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+@api_view()
+def home(request):
+    return Response({
+        "status":200,
+        "message":"this is a random message"
+    })
